1068aeb6329626a33513b0cbc4273995e4098af3
Boston Dell-Vandenberg <boston@pomelodesign.com>
2016-03-07 11:43:54 -0500

    Customize templates

--- config/templates/index.js
+++ config/templates/index.js
@@ -24,17 +24,17 @@ mod.run(($templateCache) => {
     // $templateCache.put('module/about.html', require('!html!./module/about.html'));
 
     // Directives
-    // $templateCache.put('directive/author.html', require('!html!./directive/author.html'));
+    $templateCache.put('directive/author.html', require('!html!./directive/author.html'));
     // $templateCache.put('directive/comment.html', require('!html!./directive/comment.html'));
     // $templateCache.put('directive/comments.html', require('!html!./directive/comments.html'));
     // $templateCache.put('directive/taxonomies.html', require('!html!./directive/taxonomies.html'));
     // $templateCache.put('directive/emptyList.html', require('!html!./directive/emptyList.html'));
     // $templateCache.put('directive/loader.html', require('!html!./directive/loader.html'));
-    // $templateCache.put('directive/menu.html', require('!html!./directive/menu.html'));
+    $templateCache.put('directive/menu.html', require('!html!./directive/menu.html'));
     // $templateCache.put('directive/page.html', require('!html!./directive/page.html'));
     // $templateCache.put('directive/pages.html', require('!html!./directive/pages.html'));
-    // $templateCache.put('directive/post.html', require('!html!./directive/post.html'));
-    // $templateCache.put('directive/posts.html', require('!html!./directive/posts.html'));
+    $templateCache.put('directive/post.html', require('!html!./directive/post.html'));
+    $templateCache.put('directive/posts.html', require('!html!./directive/posts.html'));
     // $templateCache.put('directive/postToolbar.html', require('!html!./directive/postToolbar.html'));
 });
 
