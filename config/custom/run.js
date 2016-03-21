3d2b42cdbc20b69614f0691f85b6bcb16f89973c
Boston Dell-Vandenberg <boston@pomelodesign.com>
2016-03-21 14:14:50 -0400

    Update keyboard

--- config/custom/run.js
+++ config/custom/run.js
@@ -2,15 +2,7 @@ let mod = angular.module('wordpress-hybrid-client.overwriteRun', []);
 
 mod.run(($ionicPlatform, $window, $cordovaStatusbar, $ionicHistory) => {
     $ionicPlatform.ready(() => {
-        // Make shrink view
-        if ($window.Keyboard) {
-            $window.Keyboard.shrinkView(true);
-            $window.Keyboard.hideFormAccessoryBar(true);
-            $window.Keyboard.disableScrollingInShrinkView(false);
-        }
-        
         if ($window.cordova && $window.cordova.plugins.Keyboard) {
-            $window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
             $window.cordova.plugins.Keyboard.disableScroll(true);
         }
 
